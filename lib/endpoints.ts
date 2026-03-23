28b24e9538e3bfcb32c6535f6c9c933cb487d55c
lawaltemidayo <lawaltemidayo06@gmail.com>
2026-03-23 08:54:33 +0100

    done

--- lib/endpoints.ts
+++ lib/endpoints.ts
@@ -1,6 +1,5 @@
-export const baseURL = "https://api.solvaafrica.com/api/v1";
+﻿export const baseURL = "https://api.solvaafrica.com/api/v1";
 // export const baseURL = "https://solva-backend-prod.onrender.com/api/v1";
-
 
 export const apis = {
   login: `${baseURL}/users/admin/login`,
@@ -13,10 +12,9 @@ export const apis = {
   inno: `${baseURL}/innovations`,
   stats: `${baseURL}/stats`,
   task: `${baseURL}/tasks`,
-
+  submission: `${baseURL}/submissions`,
   past: `${baseURL}/questions`,
   docs: `${baseURL}/documents`,
-
   project: `${baseURL}/projects`,
   slider: `${baseURL}/slider`,
   notification: `${baseURL}/notification`,
